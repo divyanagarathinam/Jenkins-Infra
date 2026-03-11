1dcd320d949c8475853af6169bf9c615036369bf
divyanagarathinam <divyanagarathinam@gmail.com>
2026-03-11 15:00:46 -0400

    Modify AWS instance configuration in main.tf
    Updated security group ID and added subnet ID for AWS instance.

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-0ecb62995f68bb549"
   instance_type          = "c7i-flex.large"
   key_name               = "training_ncpl_tf"
-  vpc_security_group_ids = ["sg-06a6104d0ac9762c2"]
+  subnet_id              = "subnet-071b6fdf79d2c4741"
+  vpc_security_group_ids = ["sg-046cd16cdb7a78595"]
+
   tags = {
     Name = var.instance_names[count.index]
   }
